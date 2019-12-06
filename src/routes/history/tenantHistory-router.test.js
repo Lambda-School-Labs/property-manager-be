4d4fd9aef5b720ac079f46960d97bd27eb6d7b02
Sandy <sandy.jernigan@gmail.com>
2019-12-05 21:48:55 -0500

    Tests passed GET: '/api/history/property'

--- src/routes/history/tenantHistory-router.test.js
+++ src/routes/history/tenantHistory-router.test.js
@@ -4,43 +4,55 @@ const request = supertest(app);
 const Reset = require('../dbReset.js');
 
 // content used for tests
+let path = '/api/history/';
 
 describe('Tenant History Routes', () => {
 
-beforeEach(async () => {
-  await Reset.dbReset();
-})
+  beforeEach(async () => await Reset.dbReset());
+  afterEach(async done => done());
 
-//#region - READ
-describe('Get all tenant history results for property, by property id.', () => {
+  //#region - READ
 
-  it('should return 200 status', async done => {
-    
-    const results = await request.get('/api/history/property/1');    
-    expect(results.status).toBe(200);
-    done();
+  // GET: '/api/history/property' - Get all tenant history results for property, by property id.
+  describe('GET: \'' + path + 'property/\' endpoint', () => {
+
+    // expected input
+    const id = 1
+    path = path + 'property/' + id
+
+    it('should return 200 status', async () => {      
+      // call function  
+      try {   
+        const results = await request.get(path);   
+        expect(results.status).toBe(200);
+      } catch(err) {
+        console.log(err)
+      }
+    })
+
+    it('should return a length of 1', async () => {    
+      // call function  
+      try {   
+        const results = await request.get(path);
+        const response = await results.body;    
+        expect(response).toHaveLength(1);
+      } catch(err) {
+        console.log(err)
+      }
+    })
+
+    it('should return array', async () => {  
+      // call function  
+      try {
+        const results = await request.get(path);
+        const response = await results.body;  
+        expect(Array.isArray(response)).toBe(true);
+      } catch(err) {
+        console.log(err)
+      }
+    })
   })
 
-  it('should return array', async done => {
-    
-    const results = await request.get('/api/history/property/1');
-    const response = await results.body;
-  
-    expect(Array.isArray(response)).toBe(true);
-    done();
-  })
-
-  it('should return a length of 1', async done => {
-    
-    const results = await request.get('/api/history/property/1');
-    const response = await results.body;
-  
-    expect(response).toHaveLength(1);
-    done();
-  })
-})
-
-
-// #endregion
+  // #endregion
 
 })
