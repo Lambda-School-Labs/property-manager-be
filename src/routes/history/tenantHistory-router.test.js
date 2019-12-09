05cb6490879b905bdfa83fa53fc5ef16f8d3c858
Sandy <sandy.jernigan@gmail.com>
2019-12-08 22:20:19 -0500

    testing for add a new entry for tenant history

--- src/routes/history/tenantHistory-router.test.js
+++ src/routes/history/tenantHistory-router.test.js
@@ -17,6 +17,53 @@ describe('Tenant History Routes', () => {
   afterAll(() => Reset.close());
   afterEach(async done => done());
 
+  //#region - CREATE
+  
+  // POST: '/api/history/' - add a new entry for tenant history, returns entry added
+  path = baseRoute;
+
+  describe ('POST: \'' + path + '\' endpoint', () => {
+
+    // expected input
+    const input = {
+      'tenantId': 3,
+      'propertyId': 5,
+      'historyStartdate': "01-01-2001",
+      'historyEnddate': "12-31-2006"
+    }
+    const input2 = {
+      'tenantId': 5,
+      'propertyId': 1,
+      'historyStartdate': null
+    }
+
+    it('should return 201 status', async () => {
+      expect.assertions(1);
+      try {
+        // call function
+        const results = await request.post(path).send(input);
+        // expected results
+        expect(results.status).toBe(201);
+        // catch error
+      } catch(err) { console.log(err) }
+    })
+
+    it('should return an object', async () => {
+      expect.assertions(1);
+      try {
+        // call function
+        const results = await request.post(path).send(input2);
+        const response = await results.body;
+        // expected results
+        expect(typeof response).toBe('object');
+        // catch error
+      } catch(err) { console.log(err) }
+    })
+
+  })
+
+  //#endregion - CREATE 
+
   //#region - READ
 
   // GET: '/api/history/property' - Get all tenant history results for property, by property id.
